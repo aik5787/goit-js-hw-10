9aa91c839dbcee1b57a65dd39f290e330b0afada
Aleksandr <aik5787@gmail.com>
2023-07-28 15:06:35 -0400

    updating

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_y93JF5d0g1wRGcfrQtzrygacDWrb0A3StZkiB2CW48e50u7WneMRxFx9q5nyIgQt";
+
+
 import { fetchBreeds, fetchCatByBreed } from "./cat-api.jss"
 
 
