2d70b3b76844427f9b8d7acd11b8f8bfff7b18d0
Aleksandr <aik5787@gmail.com>
2023-07-28 15:08:38 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+
+
+// import { fetchBreeds, fetchCatByBreed } from "./cat-api.jss"
+
+
+const breedSelect = document.querySelector(".breed-select");
+
+
 import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_y93JF5d0g1wRGcfrQtzrygacDWrb0A3StZkiB2CW48e50u7WneMRxFx9q5nyIgQt";
 
 
-import { fetchBreeds, fetchCatByBreed } from "./cat-api.jss"
+function fetchBreeds() {
+  const url = "https://api.thecatapi.com/v1/breeds";
+  return axios.get(url)
+    .then(response => response.data)
+    .catch(error => {
+      throw new Error(error);
+    });
+}
 
 
-const breedSelect = document.querySelector(".breed-select");
+function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  return axios.get(url)
+    .then(response => response.data)
+    .catch(error => {
+      throw new Error(error);
+    });
+}
