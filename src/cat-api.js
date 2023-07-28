6820e6879150fef239aa094e21a872c8f7ef87ab
Aleksandr <aik5787@gmail.com>
2023-07-28 15:05:16 -0400

    updating

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+// import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "live_y93JF5d0g1wRGcfrQtzrygacDWrb0A3StZkiB2CW48e50u7WneMRxFx9q5nyIgQt";
+// axios.defaults.headers.common["x-api-key"] = "live_y93JF5d0g1wRGcfrQtzrygacDWrb0A3StZkiB2CW48e50u7WneMRxFx9q5nyIgQt";
 
 
 export function fetchBreeds() {
